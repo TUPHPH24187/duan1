4ca915d16829ae01d6811f465eb74e04a37861b1
LAPTOP-NPKCU02M\concua <tuphph24187@fpt.edu.vn>
2022-11-09 20:42:40 +0700

    no message

--- src/main/java/model/HinhThucThanhToan.java
+++ src/main/java/model/HinhThucThanhToan.java
@@ -4,10 +4,57 @@
  */
 package model;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author concu
  */
-public class HinhThucThanhToan {
+@Entity
+@Table(name = "HinhThucThanhToan")
+public class HinhThucThanhToan implements Serializable{
+    
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MaHTTT")
+    private Integer MaHTTT;
+    
+    @Column(name = "TenHTTT")
+    private String TenHTTT;
+
+    
+    
+    public Integer getMaHTTT() {
+        return MaHTTT;
+    }
+
+    public void setMaHTTT(Integer MaHTTT) {
+        this.MaHTTT = MaHTTT;
+    }
+
+    public HinhThucThanhToan() {
+    }
+
+    public HinhThucThanhToan(Integer MaHTTT, String TenHTTT) {
+        this.MaHTTT = MaHTTT;
+        this.TenHTTT = TenHTTT;
+    }
+
+    public String getTenHTTT() {
+        return TenHTTT;
+    }
+
+    public void setTenHTTT(String TenHTTT) {
+        this.TenHTTT = TenHTTT;
+    }
+    
+    
     
 }

c791388d4558985263eb79a9479b04722cecac76
LAPTOP-NPKCU02M\concua <tuphph24187@fpt.edu.vn>
2022-11-09 11:03:20 +0700

    cmt update

--- src/main/java/view/QLSanPham.java
+++ src/main/java/view/QLSanPham.java
@@ -244,7 +244,6 @@ public class QLSanPham extends javax.swing.JPanel {
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(2, 2, 2)
                         .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(80, 80, 80)
@@ -263,9 +262,7 @@ public class QLSanPham extends javax.swing.JPanel {
                                     .addComponent(jLabel8)
                                     .addComponent(txtMaGiay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(18, 18, 18)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(cbSize, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel11)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                             .addComponent(jLabel2)
@@ -281,7 +278,10 @@ public class QLSanPham extends javax.swing.JPanel {
                                         .addGap(18, 18, 18)
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                             .addComponent(jLabel12)
-                                            .addComponent(txtGiamGia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))))
+                                            .addComponent(txtGiamGia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(cbSize, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabel11))))))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(32, 32, 32)
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -335,40 +335,7 @@ public class QLSanPham extends javax.swing.JPanel {
     }//GEN-LAST:event_btnThemActionPerformed
 
     private void btnSuaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSuaActionPerformed
-        ChiTietSanPham ctsp = new ChiTietSanPham();
-
-        String tenSP = txtTenGiay.getText();
-
-        String soLuong = txtSoLuong.getText();
-        Integer soLuongStr = Integer.parseInt(soLuong);
-
-        String gia = txtGia.getText();
-        Double giaS = Double.parseDouble(gia);
-        BigDecimal giaStr = BigDecimal.valueOf(giaS);
-
-        String giamGia = txtGiamGia.getText();
-        Double giamGiaS = Double.parseDouble(giamGia);
-        BigDecimal giamgiaStr = BigDecimal.valueOf(giamGiaS);
-
-        XuatXu xuatXu = (XuatXu) cbXuatXu.getSelectedItem();
-        ctsp.setXuatXu(xuatXu);
-
-        KichThuoc kichThuoc = (KichThuoc) cbSize.getSelectedItem();
-        ctsp.setKichThuoc(kichThuoc);
-
-        ChatLieu chatLieu = (ChatLieu) cbChatLieu.getSelectedItem();
-        ctsp.setChatLieu(chatLieu);
-
-        ctsp.setTrangThai(rbHoatDong.isSelected() ? 1 : 0);
-
-        ctsp.setTenCTSP(tenSP);
-        ctsp.setSoLuong(soLuongStr);
-        ctsp.setGia(giaStr);
-        ctsp.setGiamGia(giamgiaStr);
-
-        String result = quanLySanPhamService.updateCTSanPham(ctsp);
-        JOptionPane.showMessageDialog(this, result);
-        loadTable(quanLySanPhamService.getListChiTietSanPham());
+       
 
     }//GEN-LAST:event_btnSuaActionPerformed
 

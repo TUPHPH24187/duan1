2b96bd71c92a1fe8f05bc19e67e32d013ec1106c
LAPTOP-NPKCU02M\concua <tuphph24187@fpt.edu.vn>
2022-11-21 13:31:20 +0700

    update class HD HDCT

--- src/main/java/DomainModels/HoaDon.java
+++ src/main/java/DomainModels/HoaDon.java
@@ -4,31 +4,163 @@
  */
 package DomainModels;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 /**
  *
  * @author concu
  */
-public class HoaDon {
+@Entity
+@Table(name = "HoaDon")
+public class HoaDon implements Serializable{
     
-    private Integer maHD;
-    
-    private KhachHang MaKH;
-    
-    private NhanVien MaNV;  
-    
-    private HinhThucThanhToan MaHTTT;
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MaHD")
+    private Integer MaHD;
+   
+    @Column(name = "NgayKhoiTao")
     private String NgayKhoiTao;
     
-    private String ghiChu;
+    @Column(name = "GhiChu")
+    private String GhiChu;
     
+    @Column(name = "trangThai")
     private int trangThai;
 
+    
+   
+    @OneToMany(mappedBy = "khachHang", fetch = FetchType.LAZY)
+    private List<KhachHang> listKhachHang;
+    
+    @OneToMany(mappedBy = "nhanVien", fetch = FetchType.LAZY)
+    private List<NhanVien> listNhanVien;
+    
+    @OneToMany(mappedBy = "hinhThucThanhToan", fetch = FetchType.LAZY)
+    private List<HinhThucThanhToan> listHinhThucThanhToan;
+   
+    
     public HoaDon() {
+        
     }
 
+    public HoaDon(Integer MaHD, String NgayKhoiTao, String GhiChu, int trangThai, List<KhachHang> listKhachHang, List<NhanVien> listNhanVien, List<HinhThucThanhToan> listHinhThucThanhToan) {
+        this.MaHD = MaHD;
+        this.NgayKhoiTao = NgayKhoiTao;
+        this.GhiChu = GhiChu;
+        this.trangThai = trangThai;
+        this.listKhachHang = listKhachHang;
+        this.listNhanVien = listNhanVien;
+        this.listHinhThucThanhToan = listHinhThucThanhToan;
+    }
+
+    /**
+     * @return the MaHD
+     */
+    public Integer getMaHD() {
+        return MaHD;
+    }
+
+    /**
+     * @param MaHD the MaHD to set
+     */
+    public void setMaHD(Integer MaHD) {
+        this.MaHD = MaHD;
+    }
+
+    /**
+     * @return the NgayKhoiTao
+     */
+    public String getNgayKhoiTao() {
+        return NgayKhoiTao;
+    }
+
+    /**
+     * @param NgayKhoiTao the NgayKhoiTao to set
+     */
+    public void setNgayKhoiTao(String NgayKhoiTao) {
+        this.NgayKhoiTao = NgayKhoiTao;
+    }
+
+    /**
+     * @return the GhiChu
+     */
+    public String getGhiChu() {
+        return GhiChu;
+    }
+
+    /**
+     * @param GhiChu the GhiChu to set
+     */
+    public void setGhiChu(String GhiChu) {
+        this.GhiChu = GhiChu;
+    }
+
+    /**
+     * @return the trangThai
+     */
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    /**
+     * @param trangThai the trangThai to set
+     */
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    /**
+     * @return the listKhachHang
+     */
+    public List<KhachHang> getListKhachHang() {
+        return listKhachHang;
+    }
+
+    /**
+     * @param listKhachHang the listKhachHang to set
+     */
+    public void setListKhachHang(List<KhachHang> listKhachHang) {
+        this.listKhachHang = listKhachHang;
+    }
+
+    /**
+     * @return the listNhanVien
+     */
+    public List<NhanVien> getListNhanVien() {
+        return listNhanVien;
+    }
+
+    /**
+     * @param listNhanVien the listNhanVien to set
+     */
+    public void setListNhanVien(List<NhanVien> listNhanVien) {
+        this.listNhanVien = listNhanVien;
+    }
+
+    /**
+     * @return the listHinhThucThanhToan
+     */
+    public List<HinhThucThanhToan> getListHinhThucThanhToan() {
+        return listHinhThucThanhToan;
+    }
+
+    /**
+     * @param listHinhThucThanhToan the listHinhThucThanhToan to set
+     */
+    public void setListHinhThucThanhToan(List<HinhThucThanhToan> listHinhThucThanhToan) {
+        this.listHinhThucThanhToan = listHinhThucThanhToan;
+    }
     
     
 }
--- src/main/java/ViewModels/HoaDonChiTiet.java
+++ src/main/java/ViewModels/HoaDonChiTiet.java
@@ -5,31 +5,146 @@
 package ViewModels;
 
 import DomainModels.HoaDon;
+import java.io.Serializable;
 import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  *
  * @author concu
  */
-public class HoaDonChiTiet {
-    
+@Entity
+@Table(name = "HoaDonChiTiet")
+public class HoaDonChiTiet implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MaHDCT")
     private Integer MaHDCT;
     
-    private HoaDon MaHD;
+    @Column(name = "SoLuong")
+    private Integer SoLuong;
     
-    private ChiTietSanPham MaCTSP;
-    
-    private Integer soLuong;
-    
+    @Column(name = "Gia")
     private BigDecimal Gia;
     
+    @Column(name = "GiamGia")
     private BigDecimal GiamGia;
     
+    @Column(name = "ThanhTien")
     private BigDecimal ThanhTien;
     
+    @Column(name = "GhiChu")
     private String GhiChu;
     
+    @Column(name = "TrangThai")
     private Integer TrangThai;
     
+    
+    @ManyToOne
+    @JoinColumn(name = "MaHD", nullable = false)
+    private HoaDon MaHD;
+        
+    @ManyToOne
+    @JoinColumn(name = "MaCTSP", nullable = false)
+    private ChiTietSanPham MaCTSP;
+
+    public HoaDonChiTiet() {
+    }
+
+    public HoaDonChiTiet(Integer MaHDCT, Integer SoLuong, BigDecimal Gia, BigDecimal GiamGia, BigDecimal ThanhTien, String GhiChu, Integer TrangThai, HoaDon MaHD, ChiTietSanPham MaCTSP) {
+        this.MaHDCT = MaHDCT;
+        this.SoLuong = SoLuong;
+        this.Gia = Gia;
+        this.GiamGia = GiamGia;
+        this.ThanhTien = ThanhTien;
+        this.GhiChu = GhiChu;
+        this.TrangThai = TrangThai;
+        this.MaHD = MaHD;
+        this.MaCTSP = MaCTSP;
+    }
+
+    public Integer getMaHDCT() {
+        return MaHDCT;
+    }
+
+    public void setMaHDCT(Integer MaHDCT) {
+        this.MaHDCT = MaHDCT;
+    }
+
+    public Integer getSoLuong() {
+        return SoLuong;
+    }
+
+    public void setSoLuong(Integer SoLuong) {
+        this.SoLuong = SoLuong;
+    }
+
+    public BigDecimal getGia() {
+        return Gia;
+    }
+
+    public void setGia(BigDecimal Gia) {
+        this.Gia = Gia;
+    }
+
+    public BigDecimal getGiamGia() {
+        return GiamGia;
+    }
+
+    public void setGiamGia(BigDecimal GiamGia) {
+        this.GiamGia = GiamGia;
+    }
+
+    public BigDecimal getThanhTien() {
+        return ThanhTien;
+    }
+
+    public void setThanhTien(BigDecimal ThanhTien) {
+        this.ThanhTien = ThanhTien;
+    }
+
+    public String getGhiChu() {
+        return GhiChu;
+    }
+
+    public void setGhiChu(String GhiChu) {
+        this.GhiChu = GhiChu;
+    }
+
+    public Integer getTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(Integer TrangThai) {
+        this.TrangThai = TrangThai;
+    }
+
+    public HoaDon getMaHD() {
+        return MaHD;
+    }
+
+    public void setMaHD(HoaDon MaHD) {
+        this.MaHD = MaHD;
+    }
+
+    public ChiTietSanPham getMaCTSP() {
+        return MaCTSP;
+    }
+
+    public void setMaCTSP(ChiTietSanPham MaCTSP) {
+        this.MaCTSP = MaCTSP;
+    }
+
+    @Override
+    public String toString() {
+        return "HoaDonChiTiet{" + "MaHDCT=" + MaHDCT + ", SoLuong=" + SoLuong + ", Gia=" + Gia + ", GiamGia=" + GiamGia + ", ThanhTien=" + ThanhTien + ", GhiChu=" + GhiChu + ", TrangThai=" + TrangThai + ", MaHD=" + MaHD + ", MaCTSP=" + MaCTSP + '}';
+    }
     
 }
